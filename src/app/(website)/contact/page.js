9fd101859d00739d9a64440d689625fa22571ce1
dada-dev <bakaryditdada@gmail.com>
2026-08-29 17:06:06 +0000

    added ADICO Image for contact page hero section

--- src/app/(website)/contact/page.js
+++ src/app/(website)/contact/page.js
@@ -5,11 +5,19 @@ import ContactForm from '@/features/website/sections/contact/ContactForm';
 import ContactMap from '@/features/website/sections/contact/ContactMap';
 import ContactSocial from '@/features/website/sections/contact/ContactSocial';
 import { PageContainer as ContactPageContainer } from '../Home.styles';
+import PageHero from '@/features/website/sections/PageHero';
 
 export default function ContactPage() {
   return (
     <ContactPageContainer>
-      <ContactHero />
+      {/* <ContactHero /> */}
+       <PageHero 
+          subtitle="Disponible"
+          title="Contactez-nous"
+          description="Une question, un projet ? Notre équipe est à votre écoute pour échanger sur vos besoins et opportunités de collaboration."
+          bgImage="/images/ADICO-IMAGE.jfif"
+          align="center"
+        />
       <ContactForm />
       <ContactMap />
       <ContactSocial />
